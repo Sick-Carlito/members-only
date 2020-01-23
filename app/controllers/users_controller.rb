b87f5a895cd49072396013625716b4f7ca254383
sick-carlito <carlb1319@gmail.com>
2020-01-23 03:17:00 +0300

    Modified Readme file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
@@ -10,7 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:info] = "Account created successfully"
+      flash[:info] = 'Account created successfully'
       redirect_to root_path
     else
       render 'new'
@@ -19,8 +21,8 @@ class UsersController < ApplicationController
 
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                     :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 end
